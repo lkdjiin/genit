a84e011b521b4d05fdf65d0d1577666dfbd287f6
lkdjiin <xavier.nayrac@gmail.com>
2011-09-27 06:39:03 +0200

    find all pages

---
+++ lib/genit/project/pages_finder.rb
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+module Genit
+
+  # I can find the list of all page names, named from
+  # the site root.
+  class PagesFinder
+  
+    # Public: Constructor.
+    #
+    # working_dir - The String working directory, where live the project.
+    def initialize working_dir
+      @working_dir = working_dir
+      @pages_folder = File.join(@working_dir, 'pages')
+    end
+  
+    # Public: Retrieve the list of all page names.
+    #
+    # Returns an Array of String.
+    def find
+      list = Dir.glob(File.join(@pages_folder, '**/*'))
+      list.map! { |name| name.gsub(@pages_folder + '/', '')}
+      list.map { |name| name.force_html_extension }
+    end
+    
+  end
+  
+end
---
+++ spec/pages_finder_spec.rb
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+
+require './spec/helper'
+
+describe PagesFinder do
+
+  before :each do
+    @project = ProjectCreator.new('spec/project-name', 'html_5', false)
+    @project.create
+    @finder = PagesFinder.new 'spec/project-name'
+  end
+  
+  after :each do
+    clean_test_repository
+  end
+  
+  def write_file name, content
+    File.open(File.join('spec/project-name', name), "w") do |file| 
+      file.puts content
+    end
+  end
+
+  it "should retrieve the right number of pages" do
+    write_file 'pages/a.html', '<h1>a</h1>'
+    write_file 'pages/b.html', '<h1>b</h1>'
+    @finder.find.size.should == 3
+  end
+  
+  it "should name the pages from the root" do
+    write_file 'pages/a.html', '<h1>a</h1>'
+    write_file 'pages/b.html', '<h1>b</h1>'
+    list = @finder.find
+    list.include?("index.html").should be_true
+    list.include?("a.html").should be_true
+    list.include?("b.html").should be_true
+  end
+  
+  it "should take care of markdown files" do
+    write_file 'pages/a.markdown', '#a'
+    write_file 'pages/b.markdown', '#b'
+    list = @finder.find
+    list.size.should == 3
+    list.include?("index.html").should be_true
+    list.include?("a.html").should be_true
+    list.include?("b.html").should be_true
+  end
+  
+end
