5d2346c90741c5b8fba37c82084fb651e7f91c65
lkdjiin <xavier.nayrac@gmail.com>
2011-09-28 07:38:06 +0200

    refactoring

--- lib/genit/documents/sitemap.rb
+++ lib/genit/documents/sitemap.rb
@@ -9,22 +9,34 @@ module Genit
     #
     # urls - an Array of all the URLs to include in the sitemap.
     def initialize urls
-      @builder = Nokogiri::XML::Builder.new do |xml|
-        xml.urlset('xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9') do
-          urls.each do |elem|
-            xml.url do
-              xml.loc elem
-            end
-          end
-        end
-      end
+      @builder = Sitemap.builder urls
     end
   
     # Public: Get the sitemap
     #
-    # Returns
+    # Returns the sitemap as a String.
     def get
       @builder.to_xml
+    end
+    
+    private
+    
+    def self.builder urls
+      Nokogiri::XML::Builder.new { |xml| Sitemap.build_urlset xml, urls }
+    end
+    
+    def self.build_urlset xml, urls
+      xml.urlset('xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9') do
+        Sitemap.build_url_elements xml, urls
+      end
+    end
+    
+    def self.build_url_elements xml, urls
+      urls.each { |elem| Sitemap.build_loc_element xml, elem }
+    end
+    
+    def self.build_loc_element xml, elem
+      xml.url { xml.loc elem }
     end
     
   end
